4166e70fb243aaa75cc2bb1cae7fae5994bd9167
DieG02 <diegob0505@gmail.com>
2025-06-04 15:35:39 +0200

    define types for the components, api and hooks folders

---
+++ src/types/components.d.ts
@@ -0,0 +1,20 @@
+export interface SkipCardProps {
+  skip: Skip;
+  selected: boolean;
+  onSelect: () => void;
+}
+
+export interface SkipCardsProps {
+  skips: Skip[];
+  selectedSkip: Skip | null;
+  onSelectSkip: (skip: Skip) => void;
+}
+
+export interface StepProps {
+  icon: React.ReactNode;
+  label: string;
+  active?: boolean;
+  completed?: boolean;
+  first?: boolean;
+  last?: boolean;
+}
