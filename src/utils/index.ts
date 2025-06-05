0a67aafa4911e4fea7f022e4da7e69e23d748b1f
DieG02 <diegob0505@gmail.com>
2025-06-05 17:22:05 +0200

    add template-image

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -4,10 +4,9 @@ import type { SkipResponse, Skip } from "../types";
  * Transforms a raw API SkipResponse object into a frontend-friendly Skip object
  */
 export const skipFormatter = (response: SkipResponse[]): Skip[] => {
-  const template =
-    "https://images.pexels.com/photos/2362357/pexels-photo-2362357.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2";
+  const template = "./skip-template.jpg";
 
-  return response.map((data: SkipResponse) => {
+  return response.map((data: SkipResponse): Skip => {
     const totalPrice =
       data.price_before_vat + data.price_before_vat * (data.vat / 100);
 
@@ -18,8 +17,8 @@ export const skipFormatter = (response: SkipResponse[]): Skip[] => {
       image: template,
       price: parseFloat(totalPrice.toFixed(2)),
       hirePeriod: data.hire_period_days,
-      onRoad: data.allowed_on_road,
-      heavyWaste: data.allows_heavy_waste,
+      notAllowedOnRoad: !data.allowed_on_road,
+      allowsHeavyWaste: data.allows_heavy_waste,
     };
   });
 };
